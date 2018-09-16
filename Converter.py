898e3a0d69783a2bf65ca2a80310b95262e37216
AaronTheKnight <42725560+AaronTheKnight@users.noreply.github.com>
2018-09-16 01:17:24 -0700

    Add files via upload

---
+++ Converter.py
@@ -0,0 +1,7 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> Celsius = int(raw_input("Enter a temperature in Celsius: "))
+
+Fahrenheit = 9.0/5.0 * Celsius + 32
+
+print "Temperature:", Celsius, "Celsius = ", Fahrenheit, " F"
